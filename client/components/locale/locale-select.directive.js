d6cb7402df262f995ac1745942f1e79306d61819
uskksu <uskksu0720@gmail.com>
2015-05-09 09:53:14 +0900

    改行追加

--- client/components/locale/locale-select.directive.js
+++ client/components/locale/locale-select.directive.js
@@ -7,9 +7,11 @@ angular.module('angularFullstackApp')
       replace: true,
       templateUrl: 'components/locale/locale-select.html',
       controller: function ($scope) {
+
         $scope.locale  = locale.getCurrentLocale();
         $scope.locales = locale.getLocales();
         $scope.visible = $scope.locales && $scope.locales.length > 1;
+
         $scope.changeLocale = function (localeName) {
           locale.setLocaleByName(localeName);
         };
